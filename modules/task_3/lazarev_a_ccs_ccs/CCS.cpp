c33b78dec442b54a46cc914dcf0bb130ee5dfe6c
Apple228 <alekse.lazarev.01@gmail.com>
2021-12-18 16:06:50 +0300

    3

--- modules/task_3/lazarev_a_ccs_ccs/CCS.cpp
+++ modules/task_3/lazarev_a_ccs_ccs/CCS.cpp
@@ -69,67 +69,74 @@ std::vector<double> multiplication(const matrix_CCS A, const matrix_CCS B) {
   return matrix;
 }
 
-std::vector<double> parallel_multiplication(matrix_CCS A, matrix_CCS B) {
+std::vector<double> parallel_multiplication(matrix_CCS &A, matrix_CCS &B) {
   int ProcRank, ProcNum;
+  matrix_CCS cop_A = A;
+  matrix_CCS cop_B = B;
   MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
   MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 
-  MPI_Bcast(&A.cols, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&A.rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&A.non_zero, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&cop_A.cols, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&cop_A.rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&cop_A.non_zero, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-  MPI_Bcast(&B.cols, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&B.rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&B.non_zero, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-  if (ProcRank > 0) {
-    A.value.resize(A.non_zero);
-    A.Rows.resize(A.non_zero);
-    A.ColumnIndexes.resize(A.cols + 1);
-  }
-
-  MPI_Bcast(A.value.data(), A.non_zero, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  MPI_Bcast(A.Rows.data(), A.non_zero, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(A.ColumnIndexes.data(), A.cols + 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&cop_B.cols, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&cop_B.rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&cop_B.non_zero, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
   if (ProcRank > 0) {
-    B.value.resize(B.non_zero);
-    B.Rows.resize(B.non_zero);
-    B.ColumnIndexes.resize(B.cols + 1);
+    cop_A.value.resize(cop_A.non_zero);
+    cop_A.Rows.resize(cop_A.non_zero);
+    cop_A.ColumnIndexes.resize(cop_A.cols + 1);
   }
 
-  MPI_Bcast(B.value.data(), B.non_zero, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  MPI_Bcast(B.Rows.data(), B.non_zero, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(B.ColumnIndexes.data(), B.cols + 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(cop_A.value.data(), cop_A.non_zero, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Bcast(cop_A.Rows.data(), cop_A.non_zero, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(cop_A.ColumnIndexes.data(), cop_A.cols + 1, MPI_INT, 0,
+            MPI_COMM_WORLD);
+
+  if (ProcRank > 0) {
+    cop_B.value.resize(cop_B.non_zero);
+    cop_B.Rows.resize(cop_B.non_zero);
+    cop_B.ColumnIndexes.resize(cop_B.cols + 1);
+  }
+
+  MPI_Bcast(cop_B.value.data(), cop_B.non_zero, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Bcast(cop_B.Rows.data(), cop_B.non_zero, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(cop_B.ColumnIndexes.data(), cop_B.cols + 1, MPI_INT, 0,
+            MPI_COMM_WORLD);
 
   std::vector<int> bounds(ProcNum + 1);
   bounds[0] = 0;
   for (int i = 1; i < ProcNum; i++) {
-    if (i < A.cols % ProcNum) {
-      bounds[i] += A.cols / ProcNum + 1 + bounds[i - 1];
+    if (i < cop_A.cols % ProcNum) {
+      bounds[i] += cop_A.cols / ProcNum + 1 + bounds[i - 1];
     } else {
-      bounds[i] += A.cols / ProcNum + bounds[i - 1];
+      bounds[i] += cop_A.cols / ProcNum + bounds[i - 1];
     }
   }
-  bounds[ProcNum] = A.cols;
+  bounds[ProcNum] = cop_A.cols;
 
-  std::vector<double> local_matrix(A.rows * B.cols);
+  std::vector<double> local_matrix(cop_A.rows * cop_B.cols);
 
   for (int index = bounds[ProcRank]; index < bounds[ProcRank + 1]; index++) {
-    for (int ind = 0; ind < B.cols; ind++) {
-      for (int i = A.ColumnIndexes[index]; i < A.ColumnIndexes[index + 1];
+    for (int ind = 0; ind < cop_B.cols; ind++) {
+      for (int i = cop_A.ColumnIndexes[index];
+           i < cop_A.ColumnIndexes[index + 1];
            i++) {
-        for (int j = B.ColumnIndexes[ind]; j < B.ColumnIndexes[ind + 1]; j++) {
+        for (int j = cop_B.ColumnIndexes[ind]; j < cop_B.ColumnIndexes[ind + 1];
+             j++) {
           if (B.Rows[j] == index) {
-            local_matrix[A.Rows[i] * B.cols + ind] += A.value[i] * B.value[j];
+            local_matrix[cop_A.Rows[i] * cop_B.cols + ind] +=
+                cop_A.value[i] * cop_B.value[j];
           }
         }
       }
     }
   }
 
-  std::vector<double> global_matrix(A.rows * B.cols);
-  MPI_Reduce(local_matrix.data(), global_matrix.data(), A.rows * B.cols,
+  std::vector<double> global_matrix(cop_A.rows * cop_B.cols);
+  MPI_Reduce(local_matrix.data(), global_matrix.data(), cop_A.rows * cop_B.cols,
              MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
   return global_matrix;
 }
